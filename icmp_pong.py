914a53534429ad14f1c19fba983b99b42bb95bbf
黎サブ 星野 <taifu9920923@gmail.com>
2022-12-02 22:22:58 +0800

    Update

--- icmp_pong.py
+++ icmp_pong.py
@@ -2,7 +2,7 @@ from src.icmp import *
 import socket, threading
 status = [True]
 buffersize = 2**16
-last = [None]
+last = [None, None]
 
 def recv(status, last):
     try:
@@ -12,7 +12,7 @@ def recv(status, last):
             result = receive(icmp, 1024) # fetch feedback
             if result:
                 Type, code, checksum, ID, seq, data, IP = result
-                last[0] = IP
+                last[0], last[1] = IP, ID
                 send(IP, ID, data, 0) # Echo reply
                 data = data.decode("utf-8")
                 print("received:",data)
@@ -35,7 +35,7 @@ def main():
                 print("stop/exit - Stop and exit the server")
                 print("help - Show this helpful menu")
             elif last[0]:
-                send(last[0], 0x1234, cmd.encode())
+                send(last[0], last[1], cmd.encode())
         print("Server stopped")
     except Exception as e:
         raise(e)
