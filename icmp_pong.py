c605ca18afd7643dfc8fbac2f4dccc3a2be54df6
黎サブ 星野 <taifu9920923@gmail.com>
2022-12-02 16:46:30 +0800

    new

---
+++ icmp_pong.py
@@ -0,0 +1,20 @@
+from src.icmp import *
+
+def main():
+    icmp = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    icmp.bind(("0.0.0.0", 0))
+    try:
+        print(f"Fetching ping...")
+        while 1:
+            result = receive(icmp, 1024) # fetch feedback
+            if result:
+                Type, code, checksum, ID, seq, data, addr = result
+                send(data, ID, data, 0) # Echo reply
+                data = data.decode("utf-8")
+                print(data)
+        icmp.close()
+    except Exception as e:
+        raise(e)
+
+if __name__ == "__main__":
+    main()
