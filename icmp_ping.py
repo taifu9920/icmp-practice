07377aab3076cd3c298c8cafc4032151f349f93c
黎サブ 星野 <taifu9920923@gmail.com>
2022-12-02 22:14:05 +0800

    Debug

--- icmp_ping.py
+++ icmp_ping.py
@@ -1,23 +1,33 @@
 from src.icmp import *
-
+import socket, threading
 default_target = "rmh.i234.me"
 
 def main():
-    if (len(sys.argv) < 2): target = [default_target]
-    else: target = sys.argv[1:]
-    for i in target:
-        try:
-            print(f"Pinging {i}...")
-            conn = send(i, 0x1234, b"Hello world") # send ping
-            result = receive(conn, 1024) # fetch feedback
-            if result:
-                Type, code, checksum, ID, seq, data, addr = result
-                data = data.decode("utf-8")
-                print(data)
-            else: print("Timeout!")
-            conn.close()
-        except Exception as e:
-            raise(e)
+    status = True
+    if (len(sys.argv) < 2): target = default_target
+    else: target = sys.argv[1]
+    while status:
+        cmd = input("Terminal# ").lower().strip()
+        if cmd == "stop" or cmd == "exit":
+            status = False
+        elif cmd == "help":
+            print(" - Command Menu -")
+            print("stop/exit - Stop and exit the server")
+            print("help - Show this helpful menu")
+        else:
+            try:
+                print(f"Pinging {target}...")
+                conn = send(target, 0x1234, b"Hello world") # send ping
+                result = receive(conn, 1024) # fetch feedback
+                if result:
+                    Type, code, checksum, ID, seq, data, addr = result
+                    data = data.decode("utf-8")
+                    print(data)
+                else: print("Timeout!")
+                conn.close()
+            except Exception as e:
+                raise(e)
+    print("Server stopped")
 
 if __name__ == "__main__":
     main()
--- icmp_pong.py
+++ icmp_pong.py
@@ -1,11 +1,13 @@
 from src.icmp import *
+import socket, threading
+status = [True]
+buffersize = 2**16
 
-def main():
-    icmp = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-    icmp.bind(("0.0.0.0", 0))
+def recv(status):
     try:
-        print(f"Fetching ping...")
-        while 1:
+        icmp = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+        icmp.bind(("0.0.0.0", 0))
+        while status[0]:
             result = receive(icmp, 1024) # fetch feedback
             if result:
                 Type, code, checksum, ID, seq, data, IP = result
@@ -13,6 +15,24 @@ def main():
                 data = data.decode("utf-8")
                 print(data)
         icmp.close()
+    except Exception as e:
+        icmp.close()
+        raise e
+
+def main():
+    try:
+        print(f"Fetching ping...")
+        threading.Thread(target=recv, args = (status,), daemon = True).start()
+
+        while status[0]:
+            cmd = input("Terminal# ").lower().strip()
+            if cmd == "stop" or cmd == "exit":
+                status[0] = False
+            elif cmd == "help":
+                print(" - Command Menu -")
+                print("stop/exit - Stop and exit the server")
+                print("help - Show this helpful menu")
+        print("Server stopped")
     except Exception as e:
         raise(e)
 
