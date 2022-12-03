f5a3602f6f4beb30fe8047eb752a9296d5a8f89d
黎サブ 星野 <taifu9920923@gmail.com>
2022-12-03 19:52:36 +0800

    Debug

--- icmp_proxy_client.py
+++ icmp_proxy_client.py
@@ -3,14 +3,14 @@ from src.icmp import *
 
 connection_limit = 5 # non-accepted connections limit
 status = [True]
-buffersize = 2**20
+buffersize = 2**15
 TCPs = dict()
 
 def forward(conn, target, ID):
     #TCP to ICMP
     try:
         while status[0]:
-            data = conn.recv(buffersize-2**15)
+            data = conn.recv(buffersize-2**12)
             if data: 
                 #print("sending ICMP data", data, "to server with ID", ID)
                 send(target, ID, data)
--- icmp_proxy_server.py
+++ icmp_proxy_server.py
@@ -2,16 +2,16 @@ import socket, threading, sys, ssl, time
 from src.icmp import *
 
 status = [True]
-buffersize = 2**20
+buffersize = 2**15
 TCPs = dict()
 
 def forward(conn, target, ID):
     #TCP to ICMP
     try:
         while status[0]:
-            data = conn.recv(buffersize - 2**15)
-            if len(data) > buffersize - 2**15:
-                slices = [data[i:i+buffersize - 2**15] for i in range(0, len(data),buffersize - 2**15)]
+            data = conn.recv(buffersize - 2**12)
+            if len(data) > buffersize - 2**12:
+                slices = [data[i:i+buffersize - 2**12] for i in range(0, len(data),buffersize - 2**12)]
                 for i in slices:
                     send(target, ID, i, 0)
             #print("sending data", data, "To ID", ID)
